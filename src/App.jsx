eb4b65415ee9295f9f749901c28cb12c483c4cf0
Jaydeep Godhani <jaydeep@Hnvk.local>
2025-09-03 23:11:37 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Footer from "./Footer";
 import Home from "./Home";
-import Knowledge from "./Knowledge";
+// import Knowledge from "./Knowledge";
 import Navbar from "./Navbar";
-import NoMatch from "./NoMatch";
-import PostsPage from "./PostsPage";
+// import NoMatch from "./NoMatch";
+// import PostsPage from "./PostsPage";
+import { lazy, Suspense } from "react";
 import Projects from "./Projects";
 import Winnings from "./Winnings";
-import BlogPost from "./typography/BlogPost";
+// import BlogPost from "./typography/BlogPost";
+const BlogPost = lazy(() => import("./typography/BlogPost"));
+const PostsPage = lazy(() => import("./PostsPage"));
+const Knowledge = lazy(() => import("./Knowledge"));
+const NoMatch = lazy(() => import("./NoMatch"));
 
 function App() {
   return (
@@ -15,19 +20,28 @@ function App() {
       <Navbar />
 
       <div className="2xl:w-1/2 px-4 lg:w-3/4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/winnings" element={<Winnings />} />
-          <Route path="/posts" element={<PostsPage />} />
-          <Route path="/knowledge" element={<Knowledge />} />
-          <Route
-            path="/knowledge/:slug"
-            element={<BlogPost sublink={"knowledge"} />}
-          />
-          <Route path="/posts/:slug" element={<BlogPost sublink={"posts"} />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="text-primary py-12 text-xl animate-fade">Loading...</div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/winnings" element={<Winnings />} />
+            <Route path="/posts" element={<PostsPage />} />
+            <Route path="/knowledge" element={<Knowledge />} />
+            <Route
+              path="/knowledge/:slug"
+              element={<BlogPost sublink={"knowledge"} />}
+            />
+            <Route
+              path="/posts/:slug"
+              element={<BlogPost sublink={"posts"} />}
+            />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -82,7 +82,7 @@ const Footer = () => {
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             role="img"
             viewBox="0 0 24 24"
             height="1em"
@@ -99,7 +99,7 @@ const Footer = () => {
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             viewBox="0 0 512 512"
             height="1em"
             width="1em"
@@ -115,7 +115,7 @@ const Footer = () => {
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             role="img"
             viewBox="0 0 24 24"
             height="1em"
